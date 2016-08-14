707537449d049ebd028a5e1104bcc2974987e54e
Rena Goldsmith <rena.kg@gmail.com>
2016-08-14 22:01:48 +1200

    move css scripts and create starter components

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './client/components/App'
+
+console.log('welcome to my-portfolio')
+
+render(<App />, document.querySelector('main'))
