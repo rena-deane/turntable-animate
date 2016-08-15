9c49a7ca6d4a7466b62eeb5445f653cc1f40c6ec
Rena Goldsmith <rena.kg@gmail.com>
2016-08-15 23:39:10 +1200

    create type effect

--- client/components/Enter.js
+++ client/components/Enter.js
@@ -8,8 +8,24 @@ class Enter extends Component {
 
   render () {
     return (
-      <div id='enter'>
-        <img src='../../public/images/on-button.svg' />
+      <div id='enter-wrapper'>
+        <div className='css-typing'>
+
+          <p className='type1'>
+            Want to listen to
+          </p>
+
+          <p className='type2'>
+            some sweet jams?<span className='blink'>|</span>
+          </p>
+
+        </div>
+
+        <div id='options'>
+          <button>Lets Jam</button>
+          <button>No Thanks</button>
+        </div>
+
       </div>
     )
   }
